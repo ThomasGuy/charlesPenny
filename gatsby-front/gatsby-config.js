a87d3eeaabb39fd1e645c45e8aa60586256cda8d
thomasguy <twguy66@gmail.com>
2021-04-10 08:59:38 +0700

    no gatsbyImageData

--- gatsby-front/gatsby-config.js
+++ gatsby-front/gatsby-config.js
@@ -32,6 +32,16 @@ module.exports = {
         overlayDrafts: !isProd && token,
       },
     },
+    // {
+    //   resolve: 'gatsby-plugin-sanity-image',
+    //   options: {
+    //     ...clientConfig.sanity,
+    //     token,
+    //     watchMode: !isProd,
+    //     useCdn: isProd,
+    //     overlayDrafts: !isProd && token,
+    //   },
+    // },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-svg',
     'gatsby-plugin-gatsby-cloud',
