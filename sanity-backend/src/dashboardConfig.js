ec48d1aae75c91be8f7617f45cd23cd644503f0e
thomasguy <twguy66@gmail.com>
2022-03-20 15:38:50 +0000

    dashboard

--- sanity-backend/src/dashboardConfig.js
+++ sanity-backend/src/dashboardConfig.js
@@ -1,9 +1,24 @@
 export default {
   widgets: [
     {
-      name: 'gatsby',
+      name: 'netlify',
+      title: 'Charles Penny Netify deploys',
       options: {
-        sites: [{ siteUrl: 'https://preview-charlespennygallery.gtsb.io/' }],
+        sites: [
+          // {
+          //   title: 'Sanity Studio',
+          //   apiId: 'xxxxx-yyyy-zzzz-xxxx-yyyyyyyy',
+          //   buildHookId: 'xxxyyyxxxyyyyxxxyyy',
+          //   name: 'sanity-gatsby-blog-20-studio',
+          // },
+          {
+            title: 'Website',
+            apiId: '926bac00-69f9-4580-a94a-baee09204bde',
+            buildHookId: 'yyyyxxxxxyyyxxdxxx',
+            name: 'practical-brahmagupta-6ca006',
+            // url: 'https://my-sanity-deployment.com',
+          }
+        ],
       },
     },
   ],
